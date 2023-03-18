f74a00b416569fe1628d3da47f38dc5815cf153e
Caio Pereira Antunes <caioefg@gmail.com>
2023-03-18 08:17:12 -0300

    Feat: criando funcoes trak_orders

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,2 +1,49 @@
+import csv
+from collections import Counter
+
+
+def most_common_food(data, name):
+    foods = [row[1] for row in data if row[0] == name]
+    common_food = Counter(foods).most_common(1)[0][0]
+    return common_food
+
+
+def number_hamburguers(data, name):
+    result = 0
+    for item in data:
+        if item[0] == name and item[1] == 'hamburguer':
+            result += 1
+    return result
+
+
+def never_ask(data, name):
+   foods = set(line[1] for line in data if line[0] != name)
+   all = set(line[1] for line in data if line[0] == name)
+   return foods - all
+   
+
+def never_went(data, name):
+    days = set(line[2] for line in data if line[0] != name)
+    all = set(line[2] for line in data if line[0] == name)
+    return  days - all 
+
+
 def analyze_log(path_to_file):
-    raise NotImplementedError
+    result = []
+    if not path_to_file.endswith('csv'):
+        raise FileNotFoundError(f"Extensão inválida: '{path_to_file}'")
+    try:
+        with open(path_to_file, 'r') as file:
+            files = csv.reader(file)
+
+            for line in files:
+                result.append(line)
+
+        with open('data/mkt_campaign.txt', 'w') as new_file:
+            new_file.write(f"{most_common_food(result, 'maria')}\n")
+            new_file.write(f"{number_hamburguers(result, 'arnaldo')}\n")
+            new_file.write(f"{never_ask(result, 'joao')}\n")
+            new_file.write(f"{never_went(result, 'joao')}\n")
+
+    except FileNotFoundError:
+        raise FileNotFoundError(f'Arquivo inexistente: {path_to_file}')
--- src/track_orders.py
+++ src/track_orders.py
@@ -1,23 +1,35 @@
+import analyze_log as analyze
+from collections import Counter
 class TrackOrders:
     # aqui deve expor a quantidade de estoque
+    def __init__(self):
+        self.orders = []
+
     def __len__(self):
-        pass
+        return len(self.orders)
 
     def add_new_order(self, customer, order, day):
-        pass
+        return self.orders.append([customer, order, day])
 
     def get_most_ordered_dish_per_customer(self, customer):
-        pass
+        return analyze.most_common_food(self.orders, customer)
 
     def get_never_ordered_per_customer(self, customer):
-        pass
+        return analyze.never_ask(self.orders, customer)
 
     def get_days_never_visited_per_customer(self, customer):
-        pass
+        return analyze.never_went(self.orders, customer)
+
+    def count_by_days(self):
+        days_list = [item[2] for item in self.orders]
+        most_common = Counter(days_list).most_common()
+        return most_common
 
     def get_busiest_day(self):
-        pass
+        most_common = self.count_by_days()
+        return most_common[0][0]
 
     def get_least_busy_day(self):
-        
-        pass
+        most_common = self.count_by_days()
+        return most_common[-1][0]
+    
